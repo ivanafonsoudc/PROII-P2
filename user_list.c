20b2cbee3448d4e12f905583965cb3fe8b070cc0
ivanafonsoudc <ivan.afonso@udc.es>
2024-04-26 20:33:08 +0200

    ALL DONE!!!:)

--- user_list.c
+++ user_list.c
@@ -43,42 +43,42 @@ tPosU previousU(tPosU p, tListU L){
 tItemU getItemU(tPosU p, tListU L){
     tPosU aux=L;
     while(aux!=NULLU){ //Recorre la lista
-        if(aux==p){ //Si la posición es la misma que la que se busca
+        if(aux==p){ //Si la posición es la misma que la que se busca devuelve el dato
             return aux->data;
         }
-        aux=aux->next;
+        aux=aux->next; //Pasa a la siguiente posición
 
     }
 }
 
 bool createNode(tPosU *p){
-    *p= malloc(sizeof(**p));
-    return (*p!=NULLU);
+    *p= malloc(sizeof(**p)); //Crea un nodo
+    return (*p!=NULLU); //Devuelve verdadero si se ha creado correctamente
 }
 
 bool insertItemU(tItemU d, tListU *L){
     tPosU q,aux;
-    if(!createNode(&q)){
+    if(!createNode(&q)){ //Si no se ha podido crear el nodo, devuelve falso
         return false;
     }
 
     q->data=d;
     q->next=NULLU;
-    if(isEmptyListU(*L)){
+    if(isEmptyListU(*L)){ //Si la lista está vacía, el nodo se inserta en la primera posición
         *L=q;
     }else{
-        if(strcmp((*L)->data.userName,q->data.userName)>0){
+        if(strcmp((*L)->data.userName,q->data.userName)>0){ //Si el nombre de usuario es menor que el de la primera posición, se inserta en la primera posición
             q->next=*L;
             *L=q;
         }else {
-            for (aux = *L; aux->next != NULLU; aux = aux->next) {
-                if (strcmp(d.userName, aux->next->data.userName) < 0) {
+            for (aux = *L; aux->next != NULLU; aux = aux->next) { //Recorre la lista
+                if (strcmp(d.userName, aux->next->data.userName) < 0) { //Si el nombre de usuario es menor que el de la siguiente posición, se inserta en esa posición
                     q->next = aux->next;
                     aux->next = q;
                     break;
                 }
             }
-            if(aux->next==NULLU){
+            if(aux->next==NULLU){ //Si no se ha insertado en ninguna posición, se inserta en la última
                 aux->next=q;
             }
 
@@ -90,25 +90,25 @@ bool insertItemU(tItemU d, tListU *L){
 
 void deleteAtPositionU(tPosU p,tListU *L) {
     tPosU aux;
-    if (isEmptyListU(*L)) {
+    if (isEmptyListU(*L)) { //Si la lista está vacía, no se puede borrar
         return;
     }
 
-    if (p == NULLU) {
-        for (aux = *L; aux->next != NULLU; aux = aux->next) {
-            if (aux->next == lastU(*L)) {
+    if (p == NULLU) { //Si la posición es nula
+        for (aux = *L; aux->next != NULLU; aux = aux->next) {  //Recorre la lista
+            if (aux->next == lastU(*L)) { //Si la siguiente posición es la última, se borra
                 free(aux->next);
                 aux->next = NULLU;
                 break;
             }
         }
-    } else if (p == *L) {
+    } else if (p == *L) { //Si la posición es la primera, se borra
         tPosU temp = p-> next;
         free(p);
         *L = temp;
     } else {
-        for (aux = *L; aux->next != NULLU; aux = aux->next) {
-            if (aux->next == p) {
+        for (aux = *L; aux->next != NULLU; aux = aux->next) { //Recorre la lista
+            if (aux->next == p) { //Si la siguiente posición es la que se busca, se borra
                 tPosU temp = p-> next;
                 aux->next = temp;
                 break;
@@ -122,11 +122,11 @@ void deleteAtPositionU(tPosU p,tListU *L) {
 
 
 void updateItemU(tItemU d,tPosU p,tListU *L){
-    p->data=d;
+    p->data=d; //Actualiza el item, introduciendo los nuevos datos del item en la posición p
 }
 
 tPosU findItemU(tUserName d,tListU L){
     tPosU aux;
-    for(aux=firstU(L);aux!=NULLU && strcmp(aux->data.userName,d)!=0;aux=aux->next);
-    return aux;
+    for(aux=firstU(L);aux!=NULLU && strcmp(aux->data.userName,d)!=0;aux=aux->next); //Recorre la lista
+    return aux; //Devuelve la posición del elemento
 }
